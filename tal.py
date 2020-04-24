e522d230096a0e3c7f928f93935b629db5e3ceb4
Tal Bachar <Tal@bachar.co.il>
2020-04-24 19:35:33 -0400

    before trying to create a func for ready_queue

--- tal.py
+++ tal.py
@@ -4,14 +4,22 @@
 # Prof. Pavel Shostak
 
 class Process:
-
-    def __init__(self, pid, realtime, bytes):
+    def __init__(self, pid, realtime, bytes, using_cpu):
         self.pid = pid
-        self.realtime = realtime
+        if realtime == True:
+            self.realtime = "REALTIME"
+        else:
+            self.realtime = "COMMON"
         self.bytes = bytes
+        self.using_cpu = using_cpu
 
     def pcb(self):
-        print("\t", self.pid, "\t ", self.realtime, "\t", self.bytes)
+       print("\t", self.pid, "\t", self.realtime, "\t", end=" ")
+       if self.using_cpu:
+           print("RUNNING")
+       else:
+           print("WAITING")
+
 
 
 def computer_specs():
@@ -31,23 +39,37 @@ def computer_specs():
 def main():
     RAM_memory, num_of_harddisk = computer_specs()
 
-    process_list = []
+    ready_queue = []
     pid = 0
 
     while True:
 
+        using_cpu = False
         user_input = str(input(""))
 
+        #Add common process
         if user_input.split()[0] == "A":
             pid += 1
-            process_list.append(Process(pid, False, user_input.split()[1]))
+            if (not ready_queue):
+                using_cpu = True
+
+            new_common_process = Process(pid, False, user_input.split()[1], using_cpu)
+            ready_queue.append(new_common_process)
+
+        #Add realtime process
         elif user_input.split()[0] == "AR":
             pid += 1
-            process_list.append(Process(pid, True, user_input.split()[1]))
+            using_cpu = True
+            new_rt_process = Process(pid, True, user_input.split()[1], using_cpu)
+            ready_queue.append(new_rt_process)
+
+
         elif user_input.split()[0] == "S":
             if user_input.split()[1] == "r":
-                print("\tPID \t  TYPE \t STATUS")
-                for p in process_list:
+                print("\tPID\t  TYPE\t\tSTATUS")
+                print("\t-------------------------------")
+
+                for p in ready_queue:
                     p.pcb()
 
 
